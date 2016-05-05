d8564324dc98d878c66d69156e11cc5526b03c2e
egpheel <eduardo.gil@gmail.com>
2016-05-05 23:08:48 +0100

    google+ sharing

--- resources/views/partials/_social.blade.php
+++ resources/views/partials/_social.blade.php
@@ -1,11 +1,11 @@
 <div class="social-btns">
   <div class="facebook">
-    <a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode($url) }}" target="_blank">Facebook</a>
+    <a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode($url) }}" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600'); return false;">Facebook</a>
   </div>
   <div class="twitter">
-    <a class="twitter-share-button" href="https://twitter.com/intent/tweet?url={{ urlencode($url) }}&text={{ $title  }}&hashtags=@foreach($tags as $tag){{ $tag === $tags->last() ? $tag->name : $tag->name . ',' }}@endforeach" target="_blank">Tweet</a>
+    <a class="twitter-share-button" href="https://twitter.com/intent/tweet?url={{ urlencode($url) }}&text={{ $title  }}&hashtags=@foreach($tags as $tag){{ $tag === $tags->last() ? $tag->name : $tag->name . ',' }}@endforeach" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600'); return false;">Tweet</a>
   </div>
   <div class="googleplus">
-
+    <a href="https://plus.google.com/share?url={{ urlencode($url) }}" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600'); return false;">Google+</a>
   </div>
 </div>
