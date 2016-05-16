44ccf955f59548c88b562ad933764960c0d5b223
egpheel <eduardo.gil@gmail.com>
2016-05-16 21:13:57 +0100

    changed reset password page

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,34 @@
 @extends('layout')
 
-<!-- Main Content -->
+@section('navbar')
+  @include('partials._navbar')
+@endsection
+
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Redefinir palavra-passe</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
-                        {!! csrf_field() !!}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">E-mail</label>
-
-                            <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary btn-block">
-                                    <i class="fa fa-btn fa-envelope"></i>Enviar link de redifinição de palavra-passe
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+<div class="container login-register">
+  <div class="login-register-wrap">
+    <div class="login-register-title">Redefinir palavra-passe</div>
+    @if (session('status'))
+      <div class="alert alert-success">
+        {{ session('status') }}
+      </div>
+    @endif
+    <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
+      {!! csrf_field() !!}
+      <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+        <label class="col-md-4 control-label">E-mail</label>
+        <div class="col-md-6">
+          <input type="email" class="form-control" placeholder="i.e. joao@exemplo.pt" name="email" value="{{ old('email') }}">
         </div>
-    </div>
+      </div>
+      <div class="form-group">
+        <div class="login-register-btn">
+          <button type="submit" class="btn read-more">
+            Enviar link de redifinição de palavra-passe
+          </button>
+        </div>
+      </div>
+    </form>
+  </div>
 </div>
 @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,70 +1,46 @@
 @extends('layout')
 
+@section('navbar')
+  @include('partials._navbar')
+@endsection
+
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Redefinir palavra-passe</div>
+  <div class="container login-register">
+    <div class="login-register-wrap">
+      <div class="login-register-title">Redefinir palavra-passe</div>
+      <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/reset') }}">
+        {!! csrf_field() !!}
+        <input type="hidden" name="token" value="{{ $token }}">
 
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/reset') }}">
-                        {!! csrf_field() !!}
-
-                        <input type="hidden" name="token" value="{{ $token }}">
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">E-mail</label>
-
-                            <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ $email or old('email') }}">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Palavra-passe</label>
-
-                            <div class="col-md-6">
-                                <input type="password" class="form-control" name="password">
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Confirme a palavra-passe</label>
-                            <div class="col-md-6">
-                                <input type="password" class="form-control" name="password_confirmation">
-
-                                @if ($errors->has('password_confirmation'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-refresh"></i>Redefinir palavra-passe
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+          <label class="col-md-4 control-label">E-mail</label>
+          <div class="col-md-6">
+            <input type="email" class="form-control" placeholder="i.e. joao@exemplo.pt" name="email" value="{{ $email or old('email') }}">
+          </div>
         </div>
+
+        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+          <label class="col-md-4 control-label">Palavra-passe</label>
+          <div class="col-md-6">
+            <input type="password" class="form-control" placeholder="Escolha uma palavra-passe" name="password">
+          </div>
+        </div>
+
+        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+          <label class="col-md-4 control-label">Confirme</label>
+          <div class="col-md-6">
+            <input type="password" class="form-control" placeholder="Confirme a palavra-passe" name="password_confirmation">
+          </div>
+        </div>
+
+        <div class="form-group">
+          <div class="login-register-btn">
+            <button type="submit" class="btn read-more">
+              Redefinir palavra-passe
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
-</div>
+  </div>
 @endsection
