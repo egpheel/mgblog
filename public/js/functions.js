72ab5e098cfc32842ce542ece8438547981edb59
egpheel <eduardo.gil@gmail.com>
2016-05-18 00:54:15 +0100

    adding responsiveness

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,5 +1,3 @@
-var mobileNav = false;
-
 $(document).ready(function() {
 
   $(window).scroll(function() {
@@ -30,10 +28,6 @@ $(document).ready(function() {
   comment();
   editComment();
   deleteComment();
-
-  mobileNavToggle();
-
-  $(window).resize(checkSize);
 });
 
 function stickySidebar() {
@@ -231,39 +225,4 @@ function deleteComment() {
       }
     });
   });
-}
-
-function mobileNavToggle() {
-  var btn = $('.mobile-menu-wrap');
-  var menu = $('.navbar-mobile > ul.menu');
-  var header = $('.header');
-
-  btn.on('click', function () {
-    if (mobileNav) {
-      menu.fadeOut('fast');
-      mobileNav = false;
-      header.animate({
-        marginTop: "75px"
-      });
-    } else {
-      menu.fadeIn();
-      mobileNav = true;
-      header.animate({
-        marginTop: "285px"
-      });
-    }
-  });
-}
-
-function checkSize() {
-  var check = $('.navbar-mobile');
-  var header = $('.header');
-
-  if(check.css('display') != 'block') {
-    header.css('margin-top', '75px');
-  } else {
-    if (mobileNav) {
-      header.css('margin-top', '285px');
-    }
-  }
 }
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,6 +10,9 @@
     <!--Bootstrap-->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <!--my js-->
+    <script src="/js/mobilemenu.js"></script>
+
     @yield('stylesheets')
 
     @yield('scripts')
