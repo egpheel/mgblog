5c77254643ec06cc91c79ffe4d5c966c19328ec3
egpheel <eduardo.gil@gmail.com>
2017-11-28 19:29:48 +0000

    changed logo

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -1,6 +1,7 @@
 <div class="header-bar">
 </div>
 <div class="header">
-  <h1><a href="/">Marco Gil</a></h1>
+  <!-- <h1><a href="/">Marco Gil</a></h1> -->
+  <a href="/"><img src="/img/marco_b_w.png"></a>
   <p><em>fotografias que contam histórias</em></p>
 </div>
