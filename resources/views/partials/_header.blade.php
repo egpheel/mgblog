e35d1a56547bec8cdbd20c7ca63e9f5611806e48
egpheel <eduardo.gil@gmail.com>
2017-11-28 20:25:03 +0000

    fixed image size

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -2,6 +2,6 @@
 </div>
 <div class="header">
   <!-- <h1><a href="/">Marco Gil</a></h1> -->
-  <a href="/"><img src="/img/marco_b_w_small.png"></a>
+  <h1><a href="/"><img src="/img/marco_b_w.png" style="width: 25%"></a></h1>
   <p><em>fotografias que contam histórias</em></p>
 </div>
